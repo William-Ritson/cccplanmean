ccc9cf9b2abc5578ec885fd825f2564b2d789cd5
unknown <william.ritson@gmail.com>
2015-01-16 21:59:16 -0800

    Added better loading and error indicators
    Added an loading indicator while the transfer page makes AJAX requests
    
    Added an error indicator and a retry button when something does wrong
    with one of those ajax requests.

--- public/dist/application.js
+++ public/dist/application.js
@@ -624,15 +624,23 @@ angular.module('transfer').controller('TransferController', ['$scope', '$http', 
 
         $scope.fromSchools = [];
 
+        $scope.loading = false;
+
         var init = function () {
-            $http.get('/data/assist-index.json').
+            $scope.loading = true;
+            
+            $http.get('/data/assist-index.json').            
             success(function (data, status, headers, config) {
                 $log.log(data);
                 $scope.toSchools = data.to;
                 $scope.fromSchools = data.from;
+                $scope.loading = false;
             }).
             error(function (data, status, headers, config) {
                 $log.error(status);
+                $scope.loading = false;
+                $scope.error = 'Failed to load school list.';
+                $scope.retry = init;
             });
         };
 
@@ -651,18 +659,29 @@ angular.module('transfer').controller('TransferController', ['$scope', '$http', 
                 major: $scope.major
             };
             $log.log(query);
+            $scope.loading = true;
             $http({
                 url: 'dynAgreement',
                 method: 'GET',
                 params: query
             }).
             success(function (data, status, headers, config) {
+                $scope.loading = false;
                 $log.log(data);
                 $scope.rows = data.table;
                 $scope.src = data.src;
+                if (data.src.match('ASSIST was not able to process your request.')) {
+                    $scope.error = 'Assist.org returned an empty page.';
+                    $scope.retry = $scope.getAgreement;
+                } else {
+                    $scope.error = false;
+                }
             }).
             error(function (data, status, headers, config) {
                 $log.error(status);
+                $scope.loading = false;
+                $scope.error = 'Error while loading agreement';
+                $scope.retry = $scope.getAgreement;
             });
 
         };
